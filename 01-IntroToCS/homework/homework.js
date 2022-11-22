3c431832d296a966459b15f330f95423953854e7
hugo valencia <hvb1507@hotmail.com>
2022-11-21 18:39:35 -0600

    tarea

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,34 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var pow=0;
+   var sum=0;
+   for (let index = num.length-1; index >= 0; index--) {
+      sum += 2**pow*num[index];
+      pow++;
+   }
+   return sum;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   var bin=[];
+   var str='';
+   var div=num;
+   while (div/2!==0) {
+      bin.push(div%2);
+      div=Math.floor(div/2);
+      
+      
+   }
+   bin.reverse(bin);
+   
+   for (let index = 0; index < bin.length; index++) {
+      str+=bin[index];
+      
+   }
+
+   return str;
+}
 
 module.exports = {
    BinarioADecimal,
